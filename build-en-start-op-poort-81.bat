45f41c846d29b55350b6debd82a38cfc852afc3f
QuintenDC2 <126144609+QuintenDC2@users.noreply.github.com>
2024-05-20 16:06:25 +0200

    Update build-en-start-op-poort-81.bat

--- build-en-start-op-poort-81.bat
+++ build-en-start-op-poort-81.bat
@@ -1,10 +1,10 @@
 @echo off
 REM Build de Docker image
-docker build -t demo-site:latest .
+docker build -t demo-site-image .
 
-REM Stop en verwijder een bestaande container als die bestaat
-docker stop demo-site
-docker rm demo-site
+REM Stop en verwijder een bestaande container met dezelfde naam, indien deze bestaat
+docker stop demo-site-container
+docker rm demo-site-container
 
 REM Start een nieuwe container op poort 81
-docker run -d -p 81:80 --name demo-site demo-site:latest
+docker run -d -p 81:80 --name demo-site-container demo-site-image
